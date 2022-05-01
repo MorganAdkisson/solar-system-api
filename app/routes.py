4014be121b129ed3e674226ca65035e45a9c9170
Sreymompech <sanchhen2013@gmail.com>
2022-04-30 21:36:06 -0700

    refactor code connecting to datatabse

--- app/routes.py
+++ app/routes.py
@@ -1,48 +1,49 @@
-import json
-from flask import Blueprint, jsonify, abort, make_response
-
-class Planet:
-    def __init__(self, id, name, description, num_moons):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.num_moons = num_moons
-
-planets = [
-    Planet(1, "Mercury", "closest planet to the Sun", 0), 
-    Planet(2, "Venus",  "second planet from the Sun", 0),
-    Planet(3, "Earth",  "third planet from the Sun", 1),
-    Planet(4, "Mars",  "brightness and closeness to Earth", 2), 
-    Planet(5, "Jupiter",  "fifth planet from the Sun", 67), 
-    Planet(6, "Saturn",  "sixth planet from the Sun", 62), 
-    Planet(7, "Uranus",  "seventh planet from the Sun", 27), 
-    Planet(8, "Neptune",  "farthest planet from the Sun", 14), 
-    ]
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.planets import Planets
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    planets_response = request.get_json()
+    new_planet = Planets(
+        name = planets_response["name"],
+        description = planets_response["description"],
+        num_moons = planets_response["num_moons"]
+    )
+
+    db.session.add(new_planet)
+    db.session.commit()
+    return {
+        "id": new_planet.id
+    }, 201
+
 @planets_bp.route("", methods=["GET"])
 def get_all_planets():
-    planets_response = []
+    response_body = []
+    planets = Planets.query.all()
     for planet in planets:
-        planets_response.append({
-            "id": planet.id, 
-            "name": planet.name, 
-            "description": planet.description, 
+        response_body.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
             "num_moons": planet.num_moons
         })
-    return jsonify(planets_response)
+    return jsonify(response_body)
+
 
 def validate_planet_id(planet_id):
     try:
         planet_id = int(planet_id)
     except ValueError:
         abort(make_response({"message": f"The planet id {planet_id} is invalid. The id must be integer."}, 400))
-
+    planets = Planets.query.all()
     for planet in planets:
         if planet.id == planet_id:
             return planet
     abort(make_response({"message": f"The planet id {planet_id} is not found"}, 404))
+
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def get_planet_by_id(planet_id):
